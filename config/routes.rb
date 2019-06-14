756157de9b2a289c5775a57fc4227a9c4571f98a
関根佑介 <its.never.too.late323@gmail.com>
2019-06-14 18:47:40 +0900

    新規登録画面とログインログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
+
+  post 'users/:id/update' => 'users#update'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+
+  get '/' => 'home#top'
+  get 'production' => 'home#production'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
